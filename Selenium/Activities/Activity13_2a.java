fbaa6d38428cc589f568d02e62bddeda6a0d87f6
sandhiyaKanagarajan <84060861+sandhiyaKanagarajan@users.noreply.github.com>
2022-02-10 19:49:45 +0530

    Update Activity13_2a.java

--- Selenium/Activities/Activity13_2a.java
+++ Selenium/Activities/Activity13_2a.java
@@ -1,27 +1,34 @@
+package TestActivities;
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+
+import javax.swing.text.html.HTMLDocument.Iterator;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Activity13_2a {
-    //Write Excel
+	
+	//Write Data into Excel:
+	
+	  //Write Excel
     public void writeExcel(String filePath) throws IOException {
-        //Blank workbook
-        XSSFWorkbook workbook = new XSSFWorkbook();
-
-        //Create a blank sheet
-        XSSFSheet sheet = workbook.createSheet("Sheet1");
-
-        //This data needs to be written (Object[])
-        List<String[]> data = new ArrayList<String[]>();
-        String[] heading = {"ID", "First Name", "Last Name", "Email", "Ph.No."};
+    	
+    	//Creating workbook and sheet
+    	XSSFWorkbook workbook = new XSSFWorkbook();
+    	XSSFSheet sheet = workbook.createSheet("Sheet 1");
+    	
+    	//Preparing data
+    	List<String[]> data = new ArrayList<String[]>();
+    	String[] heading = {"ID", "First Name", "Last Name", "Email", "Ph.No."};
         String[] row1 = {"1", "Satvik", "Shah", "satshah@example.com", "4537829158"};
         String[] row2 = {"2", "Avinash", "Kati", "avinashK@example.com", "4892184058"};
         String[] row3 = {"3", "Lahri", "Rath", "lahri.rath@example.com", "4528727830"};
@@ -30,85 +37,65 @@ public class Activity13_2a {
         data.add(row1);
         data.add(row2);
         data.add(row3);
-
-        //Iterate over data and write to sheet
-        int rownum = 0;
-        for (String[] rowData : data) {
-            //Creates a new row in the sheet
-            Row row = sheet.createRow(rownum++);
-            int cellnum = 0;
-            for (String cellData : rowData) {
-                //Creates a cell in the next column of that row
-                Cell cell = row.createCell(cellnum++);
-                cell.setCellValue(cellData);
-            }
-        }
-        try {
-            //this Writes to the workbook
-            FileOutputStream out = new FileOutputStream(new File(filePath));
-            workbook.write(out);
-            out.close();
-            workbook.close();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    //Read Excel
-    public void readExcel(String filePath) {
-        try {
-            FileInputStream file = new FileInputStream(filePath);
-
-            //Create Workbook instance holding reference to Excel file
-            XSSFWorkbook workbook = new XSSFWorkbook(file);
-
-            //Get first sheet from the workbook
-            XSSFSheet sheet = workbook.getSheetAt(0);
-
-            //Iterate through each rows one by one
-            Iterator<Row> rowIterator = sheet.iterator();
-            while (rowIterator.hasNext()) {
-                Row row = rowIterator.next();
-                //For each row, iterate through all the columns
-                Iterator<Cell> cellIterator = row.cellIterator();
-
-                while (cellIterator.hasNext()) {
-                    Cell cell = cellIterator.next();
-                    //Check the cell type and format accordingly
-                    switch (cell.getCellType()) {
-                    case NUMERIC: 
-                        System.out.print(cell.getNumericCellValue() + " \t ");
-                        break;
-                    case STRING: 
-                        System.out.print(cell.getStringCellValue() + " \t ");
-                        break;
-                    default:
-                        System.out.println("Invalid value");
-                        break;
-                    }
-                }
-                System.out.println("");
-            }
-            file.close();
-            workbook.close();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String args[]) throws IOException {
-        //Prepare the path of excel file
-        String filePath = "src/test/resources/sample.xlsx";
-
-        //Create an object of current class
-        Activity14_2 objExcelFile = new Activity14_2();
-
-        //Write the file using file name, sheet name and the data to be filled
-        objExcelFile.writeExcel(filePath);
         
-        //Call read file method of the class to read data
-        objExcelFile.readExcel(filePath);
+        for(String[] rowdata: data) {
+        	int rownum = 0;
+        	Row row = sheet.createRow(rownum++);
+        	
+        	int cellnum =0;
+        	for(String celldata: rowdata) {
+        		Cell cell = row.createCell(cellnum++);
+        		cell.setCellValue(celldata);
+        	}
+        }
+        FileOutputStream out = new FileOutputStream(new File(filePath));
+        workbook.write(out);
+        out.close();
+        workbook.close();
+        System.out.println("Write the data to Excel Successfully!!");
+    	
     }
+
+    //read data from Excel:
+    public void readExcel(String filepath) throws IOException {
+    	
+    FileInputStream input = new FileInputStream(filepath);
+    XSSFWorkbook workbook = new XSSFWorkbook(filepath);
+    XSSFSheet sheet = workbook.getSheetAt(0);
+    
+    java.util.Iterator<Row> rowIterator = sheet.iterator();
+    while(rowIterator.hasNext()) {
+    	Row row=rowIterator.next();
+    	
+    	java.util.Iterator<Cell> cellIterator = row.cellIterator();
+    	while(cellIterator.hasNext()) {
+    		Cell cell = cellIterator.next();
+    		switch(cell.getCellType()) {
+    		case NUMERIC:
+    			System.out.println(cell.getNumericCellValue() + " /t " );
+    			break;
+    			
+    		case STRING:	
+    			System.out.println(cell.getStringCellValue() + " /t ");
+    			break;
+    			
+    		default:
+    			System.out.println("Invalid format");
+    			break;
+    		}
+    	}
+
+    	input.close();
+    	workbook.close();
+    }
+    
+    }
+    
+	public static void main(String[] args) throws IOException {
+		String filepath = "src/test/resources/sample.xlsx";
+		Activity13_2a obj = new Activity13_2a();
+		obj.writeExcel(filepath);
+		obj.readExcel(filepath);
+	}
+
 }
